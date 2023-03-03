4e87dae3978438e6ff0c1eabb5c3274bdd33511c
Tomás Friz <tomyagustinf@gmail.com>
2023-03-03 10:39:05 -0300

    terminado las validaciones para el futureEvents y el pastEvents

---
+++ assets/js/pastEvents.js
@@ -0,0 +1,8 @@
+
+for (let i = 0; i < data.events.length; i++) {
+    if (data.events[i].date < data.currentDate) {
+        for (let key in data.events[i]) {
+            console.log(`${key}: ${data.events[i][key]}`);
+        }
+    }
+}
